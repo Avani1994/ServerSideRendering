e440af6111659b639aa3dfcfe241c1909238283f
Avani1994 <avaninith@gmail.com>
2020-10-23 21:59:39 -0600

    added an alert which shows up when div is clicked

--- client/Home.jsx
+++ client/Home.jsx
@@ -2,8 +2,13 @@ import React from "react";
 
 const Home = (props) => (
     <React.Fragment>
-        <h1>Hello, {props.name}!</h1>
+        <div style={{ width: '300px', height: '300px', background: "green", padding: '5px' }} onClick={() => alert(`Hello ${props.name}`)}>
+            <h1>Hello, {props.name}!</h1>
+            <h4>Click here for alert</h4>
+        </div>
+
     </React.Fragment>
+
 );
 
 export default Home;
